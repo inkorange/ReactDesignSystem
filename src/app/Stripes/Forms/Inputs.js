5bdc9656f7f67841a2dfef3aa0fe5af1123abbd8
Chris West <vpht64@zebra.com>
2016-11-04 10:52:00 -0400

    Added Leftnav and updates to the dropDown components.

--- src/app/Stripes/Forms/Inputs.js
+++ src/app/Stripes/Forms/Inputs.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { StripesTheme } from '../Core/Stripes'
-import {SuggestionBox} from './SuggestionBox.js'
+import {SelectPanel} from './SelectPanel.js'
 
 /* *********************************************************************************************************************
  Component TextBox
@@ -12,24 +12,27 @@ class TextBox extends StripesTheme {
     static defaultProps = {
         style:  {},
         type: 'inputs',
-        defaultChecked: false,
         value: '',
         placeholder: null,
         error: null,
         width: null,
-        suggestions: false,
+        showSuggestions: false,
         suggestionData: []
     }
 
     constructor(props) {
         super(props);
+        var suggestionItems = [];
+
         this.state = {
             style: this.getStyles(),
             active: false,
-            value: ''
+            value: '',
+            suggestionItems: []
         };
         this.onChange = this.onChange.bind(this);
         this.applyValue = this.applyValue.bind(this);
+        this.onCompleteInputBlur = this.onCompleteInputBlur.bind(this);
     }
 
     componentDidUpdate(props) {
@@ -44,19 +47,60 @@ class TextBox extends StripesTheme {
         var val = e.target.value !== '' ? e.target.value : null;
         if(val !== this.state.value) {
             this.setState({
-                value: val
+                value: val,
+                suggestionItems: this.getSuggestions(val)
             }, () => {
-                if(this.props.suggestions) {
-                    this.refs.suggestionBox.updateSearch(val);
+                if(this.props.showSuggestions && this.state.suggestionItems.length) {
+                    this.refs.selectPanel.open(false);
+                } else {
+                    this.refs.selectPanel.close();
                 }
             });
         }
     }
 
-    applyValue(val) {
-        this.setState({
-            value: val
+    getSuggestions(term) {
+        if(!term) {
+            return [];
+        }
+        term = term.toUpperCase();
+        var results = [];
+        this.props.suggestionData.map((v) => {
+            if(v.toUpperCase().indexOf(term) >= 0) {
+                results.push({
+                    label: v,
+                    value: v
+                });
+            }
         });
+
+        return this.sortResults(results);
+    }
+
+    sortResults(results) {
+        return results.sort(function(a,b) {return (a.code > b.code) ? 1 : ((b.code > a.code) ? -1 : 0);} );
+    }
+
+    applyValue(val) {
+        if(val !== undefined) {
+            this.setState({
+                value: val.value
+            }, () => {
+                this.refs.input.value = this.state.value ? this.state.value : null;
+            });
+        }
+    }
+
+    onCompleteInputBlur(e) {
+        setTimeout(() => {
+            var target = document.activeElement;
+            console.log(target.className);
+            if(target.className !== "SelectPanel" && this.props.showSuggestions) {
+                this.refs.selectPanel.close();
+            } else {
+                this.onInputBlur(e);
+            }
+        }, 1);
     }
 
     getStyles() {
@@ -78,15 +122,22 @@ class TextBox extends StripesTheme {
         return (
             <div style={this.state.style.container}>
                 <input
-                    value={this.state.value}
+                    ref="input"
                     placeholder={this.props.placeholder}
                     onClick={this.onInputClick}
-                    onBlur={this.onInputBlur}
+                    onBlur={this.onCompleteInputBlur}
                     onChange={this.onChange}
                     style={this.state.style.input}
                 />
                 <span style={this.state.active ? this.state.style.active.on : this.state.style.active.off}></span>
-                {this.props.suggestions ? <SuggestionBox ref="suggestionBox" data={this.props.suggestionData} onSelect={this.applyValue} /> : null}
+                {this.props.showSuggestions ?
+                    <SelectPanel
+                        ref="selectPanel"
+                        data={this.state.suggestionItems}
+                        onSelect={this.applyValue}
+                        onClose={this.onInputBlur}
+                    />
+                    : null}
                 <span style={this.state.style.error}>{this.props.error}</span>
             </div>
         )
@@ -101,7 +152,6 @@ class TextArea extends StripesTheme {
     static defaultProps = {
         style:  {},
         type: 'inputs',
-        defaultChecked: false,
         value: '',
         placeholder: null,
         error: null,
@@ -154,8 +204,115 @@ class TextArea extends StripesTheme {
 
 }
 
+/* *********************************************************************************************************************
+ Component DropDown
+ ********************************************************************************************************************* */
+class DropDown extends StripesTheme {
+    static defaultProps = {
+        style:  {},
+        type: 'inputs',
+        placeholder: 'Select...',
+        error: null,
+        width: null,
+        showEmpty: false,
+        onChange: () => { return false; }
+    }
+
+    constructor(props) {
+        super(props);
+        var items = [];
+        this.toggleSelect = this.toggleSelect.bind(this);
+        this.applyValue = this.applyValue.bind(this);
+        this.getValue = this.getValue.bind(this);
+        this.props.children.map((obj) => {
+            var label = obj.props.label ? obj.props.label : obj.props.children;
+            items.push({
+                checked: obj.props.defaultChecked,
+                label: label,
+                value: obj.props.value === undefined ? label : obj.props.value
+            });
+        });
+        this.state = {
+            style: this.getStyles(),
+            items: items,
+            active: false,
+            value: null,
+            label: null
+        }
+    }
+
+    componentDidUpdate(props) {
+        if(props !== this.props) {
+            this.setState({
+                style: this.getStyles()
+            });
+        }
+    }
+
+    applyValue(val) {
+        if(val !== undefined) {
+            this.setState({
+                value: val.value,
+                label: val.value ? val.label : null
+            }, () => {
+                this.refs.input.value = this.state.value ? this.state.label : null;
+            });
+        }
+    }
+
+    getValue() {
+        return this.state.value;
+    }
+
+    toggleSelect() {
+        this.onInputClick();
+        this.refs.selectPanel.open();
+    }
+
+    getStyles() {
+        var color = this.getColors()[this.props.type];
+        var spacing = this.getSpacing()[this.props.type];
+        var styleObj = this.getBaseStyling(spacing, color).inputs;
+
+        var input = {
+            cursor: 'pointer'
+        };
+
+        styleObj.input = Object.assign(styleObj.input, input);
+        styleObj.active.on = Object.assign(styleObj.active.on, styleObj.active.base);
+        styleObj.active.off = Object.assign(styleObj.active.off, styleObj.active.base);
+
+        return styleObj;
+    }
+
+
+    render() {
+        return (
+            <div style={this.state.style.container}>
+                <input
+                    ref="input"
+                    placeholder={this.props.placeholder}
+                    onClick={this.toggleSelect}
+                    style={this.state.style.input}
+                    readOnly="readonly"
+                />
+                <span style={this.state.active ? this.state.style.active.on : this.state.style.active.off}></span>
+                <SelectPanel
+                    ref="selectPanel"
+                    data={this.state.items}
+                    onSelect={this.applyValue}
+                    onClose={this.onInputBlur}
+                />
+                <span style={this.state.style.error}>{this.props.error}</span>
+            </div>
+
+        )
+    }
+
+}
+
 module.exports = {
     TextBox: TextBox,
-    TextArea: TextArea
-    //Item: Item
+    TextArea: TextArea,
+    DropDown: DropDown
 }
--- src/app/Stripes/Layout/IconMenu.js
+++ src/app/Stripes/Layout/IconMenu.js
@@ -7,7 +7,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { StripesTheme } from '../Core/Stripes'
 import {Icon} from  '../Symbols/Icon'
-import {Paper} from './Paper'
 
 export class IconMenu extends StripesTheme {
 
@@ -24,7 +23,6 @@ export class IconMenu extends StripesTheme {
         super(props);
 
         this.state = {
-            style: {},
             open: false
         };
 
@@ -33,31 +31,26 @@ export class IconMenu extends StripesTheme {
         this.toggleMenuOnBlur = this.toggleMenuOnBlur.bind(this);
     }
 
-    componentDidMount() {
+    componentWillMount() {
         this.setState({
             style: this.getStyles()
         });
     }
 
     toggleMenu(e, show, focus) {
-        console.log(show);
         this.setState({
             open: show === undefined ? !this.state.open : show
         }, () => {
             if(this.state.open) {
-                console.log('i will focus...');
                 this.refs.MenuBody.focus();
             }
             this.setState({
                 style: this.getStyles()
-            });
+            },this.forceUpdate);
         });
     }
 
     toggleMenuOnBlur(e) {
-        this.toggleMenu(null, false, false);
-        e.preventDefault();
-        e.stopImmediatePropagation;
         return false;
     }
 
@@ -70,31 +63,44 @@ export class IconMenu extends StripesTheme {
                 display: 'inline'
             },
             icon: {
-                cursor: 'pointer'
+                transition: 'all .5s',
+                cursor: 'pointer',
+                padding: spacing.padding,
+                background: this.state.open ? 'white' : 'transparent',
+                boxShadow: this.state.open ? '0 0 5px rgb(200,200,200)' : '0 0 0 rgb(200,200,200)',
+                zIndex: 1
             },
-            paper: {
+            menu: {
                 position: 'absolute',
-                top: '100%',
-                left: '0',
+                top: this.props.direction !== 'top' ? '100%' : 'auto',
+                bottom: this.props.direction === 'top' ? 'calc(100% + ' + spacing.padding*2 + 'px)' : 'auto',
+                left: this.props.direction === 'left' ? 'auto' : 0,
+                right: this.props.direction === 'left' ? 0 : 'auto',
                 transition: 'all .5s',
                 maxHeight: this.state.open ? '800px' : '0',
                 opacity: this.state.open ? '1.0' : '0.0',
                 maxWidth: this.props["max-width"],
                 overflow: 'hidden',
                 padding: 0,
-                minWidth: '200px'
+                minWidth: '200px',
+                outline: 'none',
+                userSelect: 'none',
+                MozUserSelect: 'none',
+                WebkitUserSelect: 'none',
+                msUserSelect: 'none',
+                background: 'white',
+                boxShadow: this.state.open ? '0 3px 5px rgb(200,200,200)' : '0 2px 0 rgb(200,200,200)',
+                zIndex: 2
             }
-        }
-
+        };
+        styleObj.base = Object.assign(styleObj.base, this.props.style);
         return styleObj;
     }
 
     render() {
-        var baseStyle = this.state.style.base; //this.props.style ? Object.assign(this.state.style.base, this.props.style) : this.state.style.base;
-        console.log(this.state.style.paper);
         return (
             <section
-                style={baseStyle}
+                style={this.state.style.base}
                 disabled={this.props.disabled}
             >
                 <Icon
@@ -103,13 +109,13 @@ export class IconMenu extends StripesTheme {
                     onClick={this.toggleMenu}
                     basestyle={this.state.style.icon}
                 />
-                <Paper
-                    style={this.state.style.paper}
-                    ref="MenuBody"
-                    onBlur={this.toggleMenuOnBlur}
+                <div style={this.state.style.menu}
+                     tabIndex="0"
+                     ref="MenuBody"
+                     onBlur={this.toggleMenuOnBlur}
                 >
                     {this.props.children}
-                </Paper>
+                </div>
             </section>
         )
     }
